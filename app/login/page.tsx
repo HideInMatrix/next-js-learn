97ba627440c283d8de6087d8f555adef999c6d94
HideInMatrix <davimorgancreate@gmail.com>
2024-05-19 13:25:05 +0800

    feat:update

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,6 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import LoginForm from '@/app/ui/login-form';
-import { Metadata } from 'next';
 
-export const metaData: Metadata = {
-  title: 'Login',
-};
 export default function LoginPage() {
   return (
     <main className="flex items-center justify-center md:h-screen">
